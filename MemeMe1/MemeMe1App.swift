a92a947748e53b9663be8757eb84959f84156692
guido.roos <guido.roos@iodigital.com>
2023-06-18 17:26:06 +0200

    Initial Commit

---
+++ MemeMe1/MemeMe1App.swift
@@ -0,0 +1,17 @@
+//
+//  MemeMe1App.swift
+//  MemeMe1
+//
+//  Created by Guido Roos on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemeMe1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
